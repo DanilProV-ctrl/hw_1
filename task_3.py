490b03146cacc6e73c9944741c0a80ecd1c31826
Даниил А. Половьянов <polovyanov_da@edu.kvantorium-pk.ru>
2020-02-27 10:33:45 +0900

    rep done

--- task_3.py
+++ task_3.py
@@ -5,6 +5,30 @@ from pyrob.api import *
 
 @task
 def task_3_1():
+    if wall_is_on_the_right():
+        pass
+    else:
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
+    if not wall_is_on_the_right():
+        move_right()
     pass
 
 
